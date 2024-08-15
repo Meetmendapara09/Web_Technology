9f7f11a30265f84e288ac7cb48c57c6dcc5f5108
Prince Ghoda <140962052+Prince61141@users.noreply.github.com>
2024-08-15 17:57:39 +0530

    Create login.php

---
+++ BackEnd/login.php
@@ -0,0 +1,33 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+// Redirect if already logged in
+if (isset($_SESSION['admin_name'])) {
+   header('location:admin_page.php');
+   exit();
+}
+
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $row = mysqli_fetch_array($result);
+      $_SESSION['admin_name'] = $row['name'];
+      header('location:admin_page.php');
+      exit();
+
+   }else{
+      $error[] = 'incorrect email or password!';
+   }
+   
+};
+?>

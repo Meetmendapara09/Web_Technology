90f527d86c26553956fbe6d39ac37f2f3e11df06
Meet <meetmendapara09@gmail.com>
2024-09-05 19:37:25 +0530

    Fix gitignore file to include missing uploads directory

--- BackEnd/config.php
+++ BackEnd/config.php
@@ -1,10 +1,10 @@
 <?php
     $servername ="localhost";
     $username = "root";
-    $password = "root";
+    $password = "";
     $db_name = "zenvedasync";
     
-    $conn = new mysqli($servername, $username, $password, $db_name, 3307);  
+    $conn = new mysqli($servername, $username, $password, $db_name, 3377);  
     
     if ($conn->connect_error) {
         die("Connection failed". $conn->connect_error);
--- BackEnd/edit-course.php
+++ BackEnd/edit-course.php
@@ -26,17 +26,11 @@ if (isset($_POST['update_course'])) {
     $price = floatval($_POST['course_price']);
 
 
-    $select = "SELECT * FROM teacher_profile WHERE user_id = $user_id";
-    $result = $conn->query($select);
-
-    if ($result->num_rows > 0) {
         $conn->query("UPDATE courses SET title='$title', description='$description', price='$price' WHERE id=$course_id");
 
         echo "<script>alert('Course updated successfully.'); window.alert('dashboard-teacher.php');</script>";
         exit();
-    } else {
-        echo "<script>alert('Error updating course: " . $conn->error . "');</script>";
-    }
+    
 }
 
 include '../FrontEnd/Pages/edit-course.html';

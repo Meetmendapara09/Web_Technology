b9c42595cc0b2dc86e3d58e7e853381870f7adba
Meet <meetmendapara09@gmail.com>
2024-10-11 16:03:32 +0530

    Fix gitignore file to include missing uploads directory and IDE configuration files

--- BackEnd/config.php
+++ BackEnd/config.php
@@ -4,7 +4,7 @@
     $password = "";
     $db_name = "zenvedasync";
     
-    $conn = new mysqli($servername, $username, $password, $db_name, 3307);  
+    $conn = new mysqli($servername, $username, $password, $db_name, 3377);
     
     if ($conn->connect_error) {
         die("Connection failed". $conn->connect_error);
